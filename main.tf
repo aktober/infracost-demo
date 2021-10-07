7005df049cd7d7499a4ba351b016d7ec9304dd65
Anna Popovychenko <anna.popovychenki@connectwise.com>
2021-10-07 22:52:04 +0300

    Removed access/secret

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-west-2"
-  access_key = ACCESS_KEY
-  secret_key = SECRET_KEY
+//  access_key = ACCESS_KEY
+//  secret_key = SECRET_KEY
 }
 
 data "aws_ami" "ubuntu" {
